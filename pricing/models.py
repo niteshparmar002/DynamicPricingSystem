01e9a4743803c3386d8533556aae0c9fe3d8f456
nitesh-segno <nitesh@segnotech.com>
2024-10-26 13:36:05 +0530

    update meta numbering in apps and models

--- pricing/models.py
+++ pricing/models.py
@@ -7,6 +7,9 @@ class Product(models.Model):
     def get_price(self, quantity=1):
         return self.price * quantity
 
+    class Meta:
+        verbose_name_plural = "01. Product"
+
     def __str__(self):
         return f"{self.name} - ${self.price}"
 
@@ -16,6 +19,9 @@ class SeasonalProduct(Product):
     def get_price(self):
         discount = self.price * (self.rate / 100)
         return self.price - discount
+    
+    class Meta:
+        verbose_name_plural = "02. Seasonal Product"
 
 class BulkProduct(Product):
     rate = models.DecimalField(max_digits=5, decimal_places=2, default=0, verbose_name="Bulk Discount Rate")
@@ -27,11 +33,17 @@ class BulkProduct(Product):
             price -= self.price * (self.rate / 100)
         return price * quantity
 
+    class Meta:
+        verbose_name_plural = "03. Bulk Product"
+
 class Discount(models.Model):
     title = models.CharField(max_length=100)
 
     def apply_discount(self, price):
         return price
+    
+    class Meta:
+        verbose_name_plural = "04. Discount"
     
     def __str__(self):
         return self.title
@@ -42,11 +54,17 @@ class PercentageDiscount(Discount):
     def apply_discount(self, price):
         return price - (price * (self.rate / 100))
 
+    class Meta:
+        verbose_name_plural = "05. Percentage Discount"
+
 class FixedAmountDiscount(Discount):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def apply_discount(self, price):
         return price - self.amount
+    
+    class Meta:
+        verbose_name_plural = "06. Fixed Amount Discount"
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="order_product")
@@ -58,6 +76,9 @@ class Order(models.Model):
         if self.discount:
             return self.discount.apply_discount(price)
         return price
+    
+    class Meta:
+        verbose_name_plural = "07. Order"
 
     def __str__(self):
         return f"Order of {self.quantity} {self.product.name}(s) - Total: ${self.calculate_total()}"
